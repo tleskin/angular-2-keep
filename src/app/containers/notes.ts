364fe9a4b6131526eacb817a6c756dfe3d442ed0
Tom Leskin <tleskin@gmail.com>
2016-08-03 11:26:02 -0400

    Create note-creator ui component

--- src/app/containers/notes.ts
+++ src/app/containers/notes.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
-import {NoteCard} from '../ui';
+import {NoteCard, NoteCreator} from '../ui';
 
 @Component({
   selector: 'notes-container',
   directives: [
-    NoteCard
+    NoteCard,
+    NoteCreator
   ],
   styles: [`
     .notes {
@@ -17,7 +18,7 @@ import {NoteCard} from '../ui';
   template: `
     <div class="row center-xs notes">
       <div class="col-xs-6 creator">
-        note creator here
+        <note-creator></note-creator>
       </div>
       <div class="notes col-xs-8">
         <div class="row between-xs">
