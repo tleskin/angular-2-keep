e7566a2ebd079d2c87587a4e2072e2cbda98beaa
Tom Leskin <tleskin@gmail.com>
2016-08-03 09:29:50 -0400

    Complete first component

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>
+      <h3>
+        Hello, world!
+      </h3>
+    </div>
+  `
+})
+
+export class App {}
