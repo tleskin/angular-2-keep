562b7ce45bd708b6d83b670d5abe766dc18bb70a
Tom Leskin <tleskin@gmail.com>
2016-08-03 09:45:39 -0400

    Add first container component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Main } from './containers';
 
 @Component({
   selector: 'app',
+  directives: [
+    Main
+  ],
   template: `
     <div>
-      <h3>
-        Hello, world!
-      </h3>
+      <main-container></main-container>
     </div>
   `
 })
