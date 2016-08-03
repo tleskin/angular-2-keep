989587377eb89906166ac997a4b535b4d753c5c1
Tom Leskin <tleskin@gmail.com>
2016-08-03 10:35:41 -0400

    Add note-card ui component, input decorator on note-card and click event

---
+++ src/app/containers/notes.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+import {NoteCard} from '../ui';
+
+@Component({
+  selector: 'notes-container',
+  directives: [
+    NoteCard
+  ],
+  styles: [`
+    .notes {
+      padding-top: 50px;
+    }
+    .creator {
+      margin-bottom: 40px;
+    }
+  `],
+  template: `
+    <div class="row center-xs notes">
+      <div class="col-xs-6 creator">
+        note creator here
+      </div>
+      <div class="notes col-xs-8">
+        <div class="row between-xs">
+          <note-card [note]="note"></note-card>
+        </div>
+      </div>
+    </div>
+  `
+})
+
+export class Notes {
+  note = { title: 'new note', value: 'new note value'}
+};
